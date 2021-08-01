fdd6213e7b1e285a5bb981484ae9c5c179e98476
Arthur Schreiber <schreiber.arthur@googlemail.com>
2021-08-01 19:34:27 +0000

    feat: drop deprecated bulk load APIs
    BREAKING CHANGE: This drops the `BulkLoad.addRow` and `BulkLoad.getRowStream` methods, in favor of passing an `Iterable` or `AsyncIterable` to the `Connection.execBulkLoad` method.

--- src/bulk-load.ts
+++ src/bulk-load.ts
@@ -245,10 +245,6 @@ class BulkLoad extends EventEmitter {
   /**
    * @private
    */
-  streamingMode: boolean;
-  /**
-   * @private
-   */
   table: string;
   /**
    * @private
@@ -273,10 +269,6 @@ class BulkLoad extends EventEmitter {
    */
   columnsByName: { [name: string]: Column };
 
-  /**
-   * @private
-   */
-  firstRowWritten: boolean;
   /**
    * @private
    */
@@ -351,8 +343,6 @@ class BulkLoad extends EventEmitter {
     this.callback = callback;
     this.columns = [];
     this.columnsByName = {};
-    this.firstRowWritten = false;
-    this.streamingMode = false;
 
     this.rowToPacketTransform = new RowTransform(this); // eslint-disable-line no-use-before-define
 
@@ -379,9 +369,6 @@ class BulkLoad extends EventEmitter {
    * @param scale For Numeric, Decimal, Time, DateTime2, DateTimeOffset.
   */
   addColumn(name: string, type: DataType, { output = false, length, precision, scale, objName = name, nullable = true }: ColumnOptions) {
-    if (this.firstRowWritten) {
-      throw new Error('Columns cannot be added to bulk insert after the first row has been written.');
-    }
     if (this.executionStarted) {
       throw new Error('Columns cannot be added to bulk insert after execution has started.');
     }
@@ -415,83 +402,6 @@ class BulkLoad extends EventEmitter {
     this.columns.push(column);
 
     this.columnsByName[name] = column;
-  }
-
-  /**
-   * Adds a row to the bulk insert.
-   *
-   * ```js
-   * bulkLoad.addRow({ first_name: 'Bill', last_name: 'Gates' });
-   * ```
-   *
-   * @param row An object of key/value pairs representing column name (or objName) and value.
-   *
-   * @deprecated This method is deprecated. Instead of adding rows individually, you should pass
-   *   all row objects when calling [[Connection.execBulkLoad]]. This method will be removed in the future.
-   */
-  addRow(row: { [columnName: string]: unknown }): void
-
-  /**
-   * Adds a row to the bulk insert.
-   *
-   * ```js
-   * bulkLoad.addRow('Bill', 'Gates');
-   * ```
-   *
-   * @param row If there are at least two columns, values can be passed as multiple arguments instead of an array. They
-   *   must be in the same order the columns were added in.
-   *
-   * @deprecated This method is deprecated. Instead of adding rows individually, you should pass
-   *   all row objects when calling [[Connection.execBulkLoad]]. This method will be removed in the future.
-   */
-  addRow(...row: unknown[]): void
-
-  /**
-   * Adds a row to the bulk insert.
-   *
-   * ```js
-   * bulkLoad.addRow(['Bill', 'Gates']);
-   * ```
-   *
-   * @param row An array representing the values of each column in the same order which they were added to the bulkLoad object.
-   *
-   * @deprecated This method is deprecated. Instead of adding rows individually, you should pass
-   *   all row objects when calling [[Connection.execBulkLoad]]. This method will be removed in the future.
-   */
-  addRow(row: unknown[]): void
-
-  addRow(...input: [ { [key: string]: unknown } ] | unknown[]) {
-    this.firstRowWritten = true;
-
-    let row: any;
-    if (input.length > 1 || !input[0] || typeof input[0] !== 'object') {
-      row = input;
-    } else {
-      row = input[0];
-    }
-
-    // write each column
-    if (Array.isArray(row)) {
-      this.rowToPacketTransform.write(this.columns.map((column, i) => {
-        let value = row[i];
-
-        if (this.options.validateBulkLoadParameters) {
-          value = column.type.validate(value);
-        }
-
-        return value;
-      }));
-    } else {
-      this.rowToPacketTransform.write(this.columns.map((column) => {
-        let value = row[column.objName];
-
-        if (this.options.validateBulkLoadParameters) {
-          value = column.type.validate(value);
-        }
-
-        return value;
-      }));
-    }
   }
 
   /**
@@ -646,44 +556,6 @@ class BulkLoad extends EventEmitter {
       tBuf.writeUInt32LE(0); // row count is 64 bits in >= TDS 7.2
     }
     return tBuf.data;
-  }
-
-  /**
-   * Switches the `BulkLoad` object into streaming mode and returns a
-   * [writable stream](https://nodejs.org/dist/latest-v10.x/docs/api/stream.html#stream_writable_streams)
-   * that can be used to send a large amount of rows to the server.
-   *
-   * ```js
-   * const bulkLoad = connection.newBulkLoad(...);
-   * bulkLoad.addColumn(...);
-   *
-   * const rowStream = bulkLoad.getRowStream();
-   *
-   * connection.execBulkLoad(bulkLoad);
-   * ```
-   *
-   * In streaming mode, [[addRow]] cannot be used. Instead all data rows must be written to the returned stream object.
-   * The stream implementation uses data flow control to prevent memory overload. [`stream.write()`](https://nodejs.org/dist/latest-v10.x/docs/api/stream.html#stream_writable_write_chunk_encoding_callback)
-   * returns `false` to indicate that data transfer should be paused.
-   *
-   * After that, the stream emits a ['drain' event](https://nodejs.org/dist/latest-v10.x/docs/api/stream.html#stream_event_drain)
-   * when it is ready to resume data transfer.
-   *
-   * @deprecated
-   *   This method is deprecated. Instead of writing rows to the stream returned by this method,
-   *   you can pass any object that implements the `Iterable` or `AsyncIterable` interface (e.g. a `Readable`
-   *   stream or an `AsyncGenerator`) when calling [[Connection.execBulkLoad]]. This method will be removed in the future.
-   */
-  getRowStream() {
-    if (this.firstRowWritten) {
-      throw new Error('BulkLoad cannot be switched to streaming mode after first row has been written using addRow().');
-    }
-    if (this.executionStarted) {
-      throw new Error('BulkLoad cannot be switched to streaming mode after execution has started.');
-    }
-    this.streamingMode = true;
-
-    return this.rowToPacketTransform;
   }
 
   /**
